12cb6705f18df6a2871a3c4cf5f7c65a55b6e13d
Máximo <maximogoitia277@gmail.com>
2024-05-26 15:19:18 -0300

    desafio 7

--- desafio7/app.js
+++ desafio7/app.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtener los elementos del DOM
     var precioElemento = document.getElementById("precioProducto");
     var inputNuevoPrecio = document.getElementById("nuevoPrecio");
     var botonCambiarPrecio = document.getElementById("cambiarPrecioBtn");
 
-    // Añadir un evento al botón para cambiar el precio
     botonCambiarPrecio.addEventListener("click", function() {
         var nuevoPrecio = inputNuevoPrecio.value;
         if (nuevoPrecio) {
@@ -14,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Modificar los estilos del título
     var tituloElemento = document.querySelector("#nombreProducto");
     tituloElemento.style.color = "#6895ff";
     tituloElemento.style.fontSize = "24px";
